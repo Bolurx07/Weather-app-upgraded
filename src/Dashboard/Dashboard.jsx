c254a8733661612285da425d73c616193a876e25
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-09-27 08:02:48 -0400

    Import and declared a variable for my useState

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import { useState,  useEffect } from "react"
 import dashBoardStyle from '../Dashboard/Dashboard.module.css'
+
+
 
 const Dashboard = () => {
   return (
     <div className={dashBoardStyle.mydashboard}>
         <div className={dashBoardStyle.div1}>
             <h1> Weather Dashboard</h1>
+            <p id="date"></p>
+            <p id="temp"></p>
+
+
+
+            <p>{data}</p>
         </div>
         <div className={dashBoardStyle.div2}>
-
+        <input type="search" name="search" value={searchQuery} onChange={handleSearch} />
+    
         </div>
         
     </div>
